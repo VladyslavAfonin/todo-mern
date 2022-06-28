9398d5b206ca5b0cad2aad63df7cdb53d22a5352
Vladyslav Afonin <vladyslav.afonin@valtech.com>
2022-06-28 15:15:45 +0300

    Authorization user

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser")
+
+const authRoute = require("./routes/auth")
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(cookieParser())
 
-app.get("/", (req, res) => {
-    res.send("Express server")
-})
-
-app.post("/name", (req, res) => {
-    if(req.body.name) {
-        return res.json({name: req.body.name})
-    } else {
-        return res.status(400).json({error: "No name provided"})
-    }
-})
+app.use("/api/auth", authRoute);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Connected to database!");
